c22f57faac493033c3ab3460a77fce3c5546fcc9
TATAPNHOB <dantatarinov@gmail.com>
2024-01-22 23:07:20 +0200

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace GameOfLife
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseLeave += new System.EventHandler(this.pictureBox1_MouseLeave);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.Resize += new System.EventHandler(this.pictureBox1_Resize);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 400);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 50);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(658, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Population N: 0";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(190, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X: ? Y: ?";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Advance";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 13);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(269, 13);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(68, 22);
+            this.numericUpDown1.TabIndex = 4;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(356, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(54, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "n: ? m: ?";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class Form1 : Form
+    {
+        int n; int m;
+        int max = 1000;
+        int width, height;
+        int[,] cells;
+        float cell_size;
+        int hover_x, hover_y;
+        int population;
+        public Form1()
+        {
+            InitializeComponent();
+            width = pictureBox1.Width;
+            height = pictureBox1.Height - panel1.Height;
+            pictureBox1.BackColor = Color.Black;
+            cells = new int[max, max];
+            cell_size = (float)numericUpDown1.Value;
+            n = (int)(height / cell_size);
+            m = (int)(width / cell_size);
+            hover_x = -1;
+            hover_y = -1;
+            population = 0;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            label3.Text = "n: " + n + " m: " + m;
+            for (int i = 0; i <= n + 1; i++)
+            {
+                e.Graphics.DrawLine(Pens.Gray, 0, i * cell_size, (m + 1) * cell_size, i * cell_size);
+            }
+            for (int i = 0; i <= m + 1; i++)
+            {
+                e.Graphics.DrawLine(Pens.Gray, i * cell_size, 0, i * cell_size, (n + 1) * cell_size);
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (cells[i, j] == 1)
+                    {
+                        e.Graphics.FillRectangle(Brushes.White, new Rectangle(i * (int)cell_size, j * (int)cell_size, (int)cell_size, (int)cell_size));
+                    }
+                }
+            }
+            if (hover_x != -1 && hover_y != -1)
+            e.Graphics.FillRectangle(cells[hover_x, hover_y] == 1 ? Brushes.LightGray : Brushes.Gray, new Rectangle(hover_x * (int)cell_size, hover_y * (int)cell_size, (int)cell_size, (int)cell_size));
+        }
+
+        private void Progress()
+        {
+            int[,] new_cells = new int[m, n];
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    int neighbours = GetLiveNeighbours(i, j);
+                    if (cells[i, j] == 1)
+                    {
+                        if (neighbours < 2) new_cells[i, j] = 0;
+                        else if (neighbours > 3) new_cells[i, j] = 0;
+                        else new_cells[i, j] = cells[i, j];
+                    }
+                    else
+                    {
+                        if (neighbours == 3) new_cells[i, j] = 1;
+                        else new_cells[i, j] = cells[i, j];
+                    }
+                }
+            }
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    cells[i, j] = new_cells[i, j];
+                }
+            }
+            population++;
+            label2.Text = "Population N: " + population;
+        }
+        private int GetLiveNeighbours(int i, int j)
+        {
+            int res = 0;
+
+            res += (i != 0 && j != 0) ? cells[i - 1, j - 1] : 0;
+            res += (i != 0) ? cells[i - 1, j] : 0;
+            res += (i != 0 && j != max - 1) ? cells[i - 1, j + 1] : 0;
+            res += (j != 0) ? cells[i, j - 1] : 0;
+            res += (j != max - 1) ? cells[i, j + 1] : 0;
+            res += (i != max - 1 && j != 0) ? cells[i + 1, j - 1] : 0;
+            res += (i != max - 1) ? cells[i + 1, j] : 0;
+            res += (i != max - 1 && j != max - 1) ? cells[i + 1, j + 1] : 0;
+
+            return res;
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            int x = (int)(e.X / cell_size);
+            int y = (int)(e.Y / cell_size);
+            if (cells[x, y] != 1)
+            cells[x, y] = 1;
+            else cells[x, y] = 0;
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_Resize(object sender, EventArgs e)
+        {
+            width = pictureBox1.Width;
+            height = pictureBox1.Height - panel1.Height;
+            n = (int)(height / cell_size);
+            m = (int)(width / cell_size);
+            pictureBox1.Refresh();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Progress();
+            pictureBox1.Refresh();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+                button1.Text = "Start";
+            }
+            else
+            {
+                timer1.Start();
+                button1.Text = "Stop";
+            }
+        }
+
+        private void pictureBox1_MouseLeave(object sender, EventArgs e)
+        {
+            hover_x = -1;
+            hover_y = -1;
+            pictureBox1.Refresh();
+        }
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            cell_size = (float)numericUpDown1.Value;
+            n = (int)(height / cell_size);
+            m = (int)(width / cell_size);
+            pictureBox1.Refresh();
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            hover_x = (int)(e.X / cell_size);
+            hover_y = (int)(e.Y / cell_size);
+            label1.Text = "X: " + hover_x + " Y: " + hover_y;
+            pictureBox1.Refresh();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Progress();
+            pictureBox1.Refresh();
+        }
+    }
+}
